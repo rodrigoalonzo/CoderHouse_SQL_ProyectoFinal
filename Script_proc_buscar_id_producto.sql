d371a22e2aacab33df1c4f9edcf77336c9e6f4c5
Rodrigo Javier Alonzo <113301775+rodrigoalonzo@users.noreply.github.com>
2024-03-31 20:11:51 -0300

    Delete Script_proc_buscar_id_producto.sql

--- Script_proc_buscar_id_producto.sql
+++
@@ -1,8 +0,0 @@
-CREATE DEFINER=`root`@`localhost` PROCEDURE `proc_buscar_Id_producto`(Producto_a_buscar VARCHAR(200))
-BEGIN
-
-	SELECT Producto, IdProducto
-    FROM productos
-    WHERE Producto LIKE CONCAT('%', Producto_a_buscar, '%');
-
-END
